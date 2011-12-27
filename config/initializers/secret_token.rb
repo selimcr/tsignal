35e3a70ea5b17b633a993c32487f581b66197012
Selim Díaz A <selimdiaz@gmail.com>
2011-12-26 20:39:41 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsignal::Application.config.secret_token = 'f6392014e4bc859ac04486900bbc3a97e86b0f68b5202bb45636eb401b19395dc489a7c4977ebf01cb072918981f1f5bc304173629eac9863b0d65a6ee5377ea'
